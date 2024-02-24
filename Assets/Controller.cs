44302a442d5e7bc4e4699449c38ad7a87ca8ae71
Brianwu24 <brian.wu24@ycdsbk12.ca>
2024-02-23 21:05:49 -0500

    Deleted usless imports

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Net;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.UIElements;
 using Random = System.Random;
 
 /*
--- Assets/Social_credits.cs
+++ Assets/Social_credits.cs
@@ -1,6 +1,5 @@
 using System;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class Inc_social_credits: MonoBehaviour
